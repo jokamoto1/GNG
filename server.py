8511b20feedc96b67b746f130a94e46c0b78a4a4
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-08-07 16:18:02 -0700

    a

--- server.py
+++ server.py
@@ -17,15 +17,25 @@ def index():
 
 @app.route("/create_user", methods=['POST'])  
 def create_user():
-    session['name'] = request.form['name']
-    session['location'] = request.form['location']
-    session['language'] = request.form['language']
-    session['comments'] = request.form['comments']
+    session['name'] = f"<h3> Name: <span class='text-warning'>  {request.form['name']} </span> </h3>"
+    session['location'] = f"<h3> Location: {request.form['location']} </h3>"
+    session['language'] = f"<h3> Language: {request.form['language']} </h3>"
+    session['comments'] = f"<h3> Comments: {request.form['comments']} </h3>"
     return redirect("result")
 
 @app.route("/result")
 def show_user():
-    return render_template('result.html')
-
+    print(session['name'])
+    print(session['location'])
+    print(session['language'])
+    print(session['comments'])
+    return render_template('index2.html')
+@app.route("/destroy")
+def destroy():
+    del session['name']
+    del session['location']
+    del session['language']
+    del session['comments']
+    return redirect('/')
 if __name__ == "__main__":
         app.run(debug=True)   
